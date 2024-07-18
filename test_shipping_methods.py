f75bcb93ad11175d26876815c3724d4899ba0174
Daveanand Mannie <daveanandmannie@gmail.com>
2024-07-18 15:00:10 -0400

    added persistant drivers to the unit test

--- test_shipping_methods.py
+++ test_shipping_methods.py
@@ -2,14 +2,18 @@ import unittest
 import json
 from pages import ShippingMethods
 
-class ShippingMethodtTest(unittest.TestCase):
+class ShippingMethodTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.shipping_method = ShippingMethods()
+        cls.data: dict = cls._load_test_case_json()
+        cls.shipping_method.login_staging()
+
     def setUp(self):
-        self.shipping_method = ShippingMethods()
-        self.data: dict = self._load_test_case_json()
-        self.shipping_method.login_staging()
         self.shipping_method.navigate(self.data['staging_url'])
-    
-    def _load_test_case_json(self):
+
+    @classmethod
+    def _load_test_case_json(cls):
         with open('test_json/allprefix.json', 'r') as file:
             return json.load(file)
 
@@ -17,6 +21,11 @@ class ShippingMethodtTest(unittest.TestCase):
         correct_name: str = self.data['shipping_method_name']
         name = self.shipping_method.get_name()
         self.assertEqual(name, correct_name, 'The name of the shipping method is incorrect')
+
+    def test_provider(self):
+        correct_provider: str = self.data['provider']
+        provider = self.shipping_method.get_shipping_provider()
+        self.assertEqual(provider, correct_provider, 'The "shipping provider" is inccorect')
 
 if __name__ == '__main__':
     unittest.main()
