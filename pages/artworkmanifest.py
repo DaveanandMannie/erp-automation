8e650b9274b3faae854eed8ff35f023928810f85
Daveanand Mannie <daveanandmannie@gmail.com>
2024-08-05 13:29:53 -0400

    added tests for artwork manifest settings

---
+++ pages/artworkmanifest.py
@@ -0,0 +1,28 @@
+from pages.base import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+class ManifestSettings(BasePage):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.driver.implicitly_wait(5)
+
+    def navigate_settings_location(self):
+        settings_tab: WebElement = self.driver.find_element(
+            By.CSS_SELECTOR,
+            "div.tab[data-key='mrp'] span.app_name"
+        )
+        settings_tab.click()
+        return
+
+    def get_manifest_fields(self) -> list:
+        field_list: list = []
+        list_div: WebElement = self.driver.find_element(
+            By.NAME,
+            'artwork_field_ids'
+        )
+        spans: list[WebElement] = list_div.find_elements(By.TAG_NAME, 'span')
+        for span in spans:
+            field_list.append(span.get_attribute('title'))
+        return field_list
---
+++ tests/test_artwork_manifest.py
@@ -0,0 +1,50 @@
+import json
+from glob import glob
+from time import sleep
+
+import pytest
+from pages.artworkmanifest import ManifestSettings
+from pytest import FixtureRequest
+
+
+class TestArtworkManifestSettings:
+    @pytest.fixture(scope='class')
+    def page(self, request: FixtureRequest):
+        """ Selenium driver with scraper """
+        environment: str = request.config.getoption('--environment')  # pyright: ignore[reportAssignmentType]  # noqa: E501
+        driver_arg: str = request.config.getoption('--window')  # pyright: ignore[reportAssignmentType]  # noqa: E501
+        if driver_arg:
+            page: ManifestSettings = ManifestSettings(driver_arg)
+        else:
+            page: ManifestSettings = ManifestSettings()
+
+        if environment == 'staging':
+            page.login_staging()
+        if environment == 'production':
+            page.login_prod()
+        return page
+
+    @pytest.fixture(
+        scope='class',
+        params=glob('testcases_json/artworkmanifest_settings/*.json')
+    )
+    def data(self, request: FixtureRequest, page: ManifestSettings, environment: str):  # noqa: E501
+        """Paramitize for multiple json test cases"""
+        with open(request.param, 'r') as file:
+            data = json.load(file)
+            if environment == 'staging':
+                page.navigate(data['staging_url'])
+
+            if environment == 'production':
+                page.navigate(data['production_url'])
+
+            sleep(0.5)
+            return data
+    # ============= Tests ============= #
+
+    def test_manifest_settings(self, page: ManifestSettings, data: dict):
+        correct_settings: list = data['settings']
+        settings: list = page.get_manifest_fields()
+        assert settings == correct_settings, (
+            'Artwork Manifest Settings not configured correctly'
+        )
