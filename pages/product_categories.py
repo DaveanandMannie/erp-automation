1a7f1a7f1e52423d677ead7d2e95fb529f52b951
Daveanand Mannie <daveanandmannie@gmail.com>
2024-08-18 18:54:07 -0400

    updated format for Odoo 17

--- pages/product_categories.py
+++ pages/product_categories.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import Select
 
 from pages.base import BasePage
@@ -9,94 +10,130 @@ class ProductCategory(BasePage):
         super().__init__(*args)
         self.driver.implicitly_wait(5)
 
-    def get_name(self) -> str:
-        name_elem = self.driver.find_element(By.ID, 'name_0')
-        name: str = name_elem.get_attribute('value')  # type: ignore
+    def get_name(self) -> str | None:
+        name_elem: WebElement = self.driver.find_element(By.ID, 'name_0')
+        name: str | None = name_elem.get_attribute('value')
         return name
 
-    def get_parent_category(self) -> str:
-        category_elem = self.driver.find_element(By.ID, 'parent_id_0')
-        category: str = category_elem.get_attribute('value')  # type: ignore
+    def get_parent_category(self) -> str | None:
+        category_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'parent_id_0'
+        )
+        category: str | None = category_elem.get_attribute('value')
         return category
 
     def get_general_category(self) -> bool:
-        category_elem = self.driver.find_element(By.ID, 'is_general_category_0')  # noqa: E501
+        category_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'is_general_category_0'
+        )
         category: bool = category_elem.is_selected()
         return category
 
     def get_require_pallet(self) -> bool:
-        pallet_elem = self.driver.find_element(By.ID, 'required_pallet_type_0')
+        pallet_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'required_pallet_type_0'
+        )
         pallet: bool = pallet_elem.is_selected()
         return pallet
 
     def get_require_profile(self) -> bool:
-        profile_elem = self.driver.find_element(
+        profile_elem: WebElement = self.driver.find_element(
             By.ID, 'required_print_profile_0'
         )
         profile: bool = profile_elem.is_selected()
         return profile
 
     def get_require_batching(self) -> bool:
-        batching_elem = self.driver.find_element(By.ID, 'is_batching_required_0')  # noqa: E501
+        batching_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'is_batching_required_0'
+        )
         batching: bool = batching_elem.is_selected()
         return batching
 
     def get_require_binning(self) -> bool:
-        binning_elem = self.driver.find_element(By.ID, 'is_binning_required_0')
+        binning_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'is_binning_required_0'
+        )
         binning: bool = binning_elem.is_selected()
         return binning
 
     def get_bin_by(self) -> str:
-        by_elem = self.driver.find_element(By.ID, 'product_bin_by_0')
-        by_option = Select(by_elem).first_selected_option
+        by_el: WebElement = self.driver.find_element(By.ID, 'product_bin_by_0')
+        by_option = Select(by_el).first_selected_option
         by: str = by_option.text
         return by
 
     # ============ Logistcs ============ #
 
-    def get_routes(self) -> list:
-        routes_elem = self.driver.find_element(By.NAME, 'route_ids')
-        routes: list = routes_elem.text.split('\n')
+    def get_routes(self) -> list[str]:
+        routes_elem: WebElement = self.driver.find_element(
+            By.NAME,
+            'route_ids'
+        )
+        routes: list[str] = routes_elem.text.split('\n')
         return routes
 
     def get_total_routes(self) -> list:
-        total_elem = self.driver.find_element(By.NAME, 'total_route_ids')
+        total_elem: WebElement = self.driver.find_element(
+            By.NAME,
+            'total_route_ids'
+        )
         total_routes: list = total_elem.text.split('\n')
         return total_routes
 
-    def get_removal_strategy(self) -> str:
-        strat_elem = self.driver.find_element(By.ID, 'removal_strategy_id_0')
-        strat: str = strat_elem.get_attribute('value')  # type: ignore[ReportAssignment]  # noqa: E501
+    def get_removal_strategy(self) -> str | None:
+        strat_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'removal_strategy_id_0'
+        )
+        strat: str | None = strat_elem.get_attribute('value')
         return strat
 
     # ============ inventory valuation ============ #
 
     def get_costing_methods(self) -> str:
-        cost_elem = self.driver.find_element(By.ID, 'property_cost_method_0')
+        cost_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'property_cost_method_0'
+        )
         cost_option = Select(cost_elem).first_selected_option
         cost: str = cost_option.text
         return cost
 
     def get_valuation(self) -> str:
-        val_elem = self.driver.find_element(By.ID, 'property_valuation_0')
+        val_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'property_valuation_0'
+        )
         selected_val = Select(val_elem).first_selected_option
         val: str = selected_val.text
         return val
 
     # ============ printing details ============ #
 
-    def get_print_profile(self) -> str:
-        profile_elem = self.driver.find_element(By.ID, 'print_profile_0')
-        profile: str = profile_elem.get_attribute('value')  # type: ignore[ReportAssignment]  # noqa: E501
+    def get_print_profile(self) -> str | None:
+        profile_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'print_profile_0'
+        )
+        profile: str | None = profile_elem.get_attribute('value')
         return profile
 
-    def get_pallet_type(self) -> str:
-        pallet_elem = self.driver.find_element(By.ID, 'pallet_type_0')
-        pallet: str = pallet_elem.get_attribute('value')  # type: ignore[ReportAssignment]  # noqa: E501
+    def get_pallet_type(self) -> str | None:
+        pal_elem: WebElement = self.driver.find_element(By.ID, 'pallet_type_0')
+        pallet: str | None = pal_elem.get_attribute('value')
         return pallet
 
-    def get_file_name(self) -> str:
+    def get_file_name(self) -> str | None:
         # its spelt wrong on the back end so this may fail if they type check
-        file_elem = self.driver.find_element(By.ID, 'artwork_naming_formate_0')
-        file: str = file_elem.get_attribute('value')  # type: ignore[ReportAssignment]  # noqa: E501
+        file_elem: WebElement = self.driver.find_element(
+            By.ID,
+            'artwork_naming_formate_0'
+        )
+        file: str | None = file_elem.get_attribute('value')
         return file
--- tests/configs/test_product_categories.py
+++ tests/configs/test_product_categories.py
@@ -53,84 +53,84 @@ class TestFinishedProductCategories:
     # ============= Tests ============= #
 
     def test_name(self, page: ProductCategory, data: dict):
-        correct_name: str = data['name']
-        name: str = page.get_name()
-        assert name == correct_name, f'Category name is not configured correctly: {data['name']}'  # noqa: E501
+        correct_val: str = data['name']
+        name: str | None = page.get_name()
+        assert name == correct_val, f'Incorrect Category name: {data['name']}'
 
     def test_parent_category(self, page: ProductCategory, data: dict):
         correct_cat: str = data['parent_category']
-        cat: str = page.get_parent_category()
-        assert cat == correct_cat, f'Parent category is not configured correct: {data['name']}'  # noqa: E501
+        cat: str | None = page.get_parent_category()
+        assert cat == correct_cat, f'Incorrect Parent category: {data['name']}'
 
     def test_general_categoroy(self, page: ProductCategory, data: dict):
-        correct_val: str = data['is_general_category']
+        cor_val: str = data['is_general_category']
         val: str = str(page.get_general_category())
-        assert val == correct_val, f'General category is not configured correctly: {data['name']}'  # noqa: E501
+        assert val == cor_val, f'Incorrect General category: {data['name']}'
 
     def test_require_pallet(self, page: ProductCategory, data: dict):
         correct_val: str = data['required_pallet']
         val: str = str(page.get_require_pallet())
-        assert val == correct_val, f'Require pallet is not configured correctly: {data['name']}'  # noqa: E501
+        assert val == correct_val, f'Incorrect Require pallet: {data['name']}'
 
     def test_require_profile(self, page: ProductCategory, data: dict):
-        correct_val: str = data['required_profile']
+        cor_val: str = data['required_profile']
         val: str = str(page.get_require_profile())
-        assert val == correct_val, f'Require print profile is not configured correctly: {data['name']}'  # noqa: E501
+        assert val == cor_val, f'Incorrect Require profile: {data['name']}'
 
     def test_require_batching(self, page: ProductCategory, data: dict):
-        correct_val: str = data['required_batching']
+        cor_val: str = data['required_batching']
         val: str = str(page.get_require_batching())
-        assert val == correct_val, f'Require batching is not configured correctly: {data['name']}'  # noqa: E501
+        assert val == cor_val, f'Incorrect Require batching: {data['name']}'
 
     def test_require_binning(self, page: ProductCategory, data: dict):
         correct_val: str = data['required_binning']
         val: str = str(page.get_require_binning())
-        assert val == correct_val, f'Require binning is not configured correctly:{data['name']}'  # noqa: E501
+        assert val == correct_val, f'Incorrect Require binning:{data['name']}'
 
     def test_bin_by(self, page: ProductCategory, data: dict):
         correct_val: str = data['product_bin_by']
         val: str = page.get_bin_by()
-        assert val == correct_val, f'Product bin by is not configured correctly:{data['name']}'  # noqa: E501
+        assert val == correct_val, f'Incorrect Product bin by:{data['name']}'
 
     def test_routes(self, page: ProductCategory, data: dict):
         correct_list: list = data['routes']
         route_list: list = page.get_routes()
-        assert route_list == correct_list, f'Routes is not configured correctly:{data['name']}'  # noqa: E501
+        assert route_list == correct_list, f'Inccorect Routes:{data['name']}'
 
     def test_total_routes(self, page: ProductCategory, data: dict):
-        correct_list: list = data['total_routes']
+        cor_val: list = data['total_routes']
         route_list: list = page.get_total_routes()
-        assert route_list == correct_list, f' Total routes is not configured correctly:{data['name']}'  # noqa: E501
+        assert route_list == cor_val, f'Incorrect Total routes: {data['name']}'
 
     def test_removal_strat(self, page: ProductCategory, data: dict):
-        correct_val: str = data['removal_strategy']
-        val: str = page.get_removal_strategy()
-        assert val == correct_val, f'Removal strategy is not configured correctly:{data['name']}'  # noqa: E501
+        cor_val: str = data['removal_strategy']
+        val: str | None = page.get_removal_strategy()
+        assert val == cor_val, f'Incorrect Removal strategy: {data['name']}'
 
     def test_cost_method(self, page: ProductCategory, data: dict):
         correct_val: str = data['costing_method']
         val: str = page.get_costing_methods()
-        assert val == correct_val, f'Costing method is not configured correctly:{data['name']}'  # noqa: E501
+        assert val == correct_val, f'Incorrect Costing method: {data['name']}'
 
     def test_inventory_valuation(self, page: ProductCategory, data: dict):
-        correct_val: str = data['inventory_valuation']
+        cor_val: str = data['inventory_valuation']
         val: str = page.get_valuation()
-        assert val == correct_val, f'Inventory valuation is not configured correctly:{data['name']}'  # noqa: E501
+        assert val == cor_val, f'Incorrect Inventory valuation: {data['name']}'
 
     def test_print_profile(self, page: ProductCategory, data: dict):
         correct_val: str = data['print_profile']
-        val: str = page.get_print_profile()
-        assert val == correct_val, f'Print profile is not configured correctly:{data['name']}'  # noqa: E501
+        val: str | None = page.get_print_profile()
+        assert val == correct_val, f'Incorrect Print profile: {data['name']}'
 
     def test_pallet_type(self, page: ProductCategory, data: dict):
         correct_val: str = data['pallet_type']
-        val: str = page.get_pallet_type()
-        assert val == correct_val, f'Pallet type is not configured correctly:{data['name']}'  # noqa: E501
+        val: str | None = page.get_pallet_type()
+        assert val == correct_val, f'Incorrect Pallet type: {data['name']}'
 
     def test_file_format(self, page: ProductCategory, data: dict):
-        correct_val: str = data['filename_format']
-        val: str = page.get_file_name()
-        assert val == correct_val, f'File name format is not configured correctly:{data['name']}'  # noqa: E501
+        cor_val: str = data['filename_format']
+        val: str | None = page.get_file_name()
+        assert val == cor_val, f'Incorrect File name format: {data['name']}'
 
 
 class TestRawProductCategories:
@@ -179,23 +179,23 @@ class TestRawProductCategories:
 
     def test_name(self, page: ProductCategory, data: dict):
         correct_name: str = data['name']
-        name: str = page.get_name()
-        assert name == correct_name, f'Category name is not configured correctly: {data['name']}'  # noqa: E501
+        name: str | None = page.get_name()
+        assert name == correct_name, f'Incorrect Category name: {data['name']}'
 
     def test_parent_category(self, page: ProductCategory, data: dict):
         correct_cat: str = data['parent_category']
-        cat: str = page.get_parent_category()
-        assert cat == correct_cat, f'Parent category is not configured correct: {data['name']}'  # noqa: E501
+        cat: str | None = page.get_parent_category()
+        assert cat == correct_cat, f'Incorrect Parent category: {data['name']}'
 
     def test_general_categoroy(self, page: ProductCategory, data: dict):
-        correct_val: str = data['is_general_category']
+        cor_val: str = data['is_general_category']
         val: str = str(page.get_general_category())
-        assert val == correct_val, f'General category is not configured correctly: {data['name']}'  # noqa: E501
+        assert val == cor_val, f'Incorrect General category: {data['name']}'
 
     def test_routes(self, page: ProductCategory, data: dict):
         correct_list: list = data['routes']
         route_list: list = page.get_routes()
-        assert route_list == correct_list, f'Routes is not configured correctly:{data['name']}'  # noqa: E501
+        assert route_list == correct_list, f'Incorrect Routes: {data['name']}'
 
     def test_total_routes(self, page: ProductCategory, data: dict):
         if data['name'] == 'Shipping':
@@ -209,31 +209,31 @@ class TestRawProductCategories:
         if data['name'] == 'Finished':
             pytest.skip('Skipping total routes test on "Finished"')
 
-        correct_list: list = data['total_routes']
-        route_list: list = page.get_total_routes()
-        assert route_list == correct_list, f' Total routes is not configured correctly:{data['name']}'  # noqa: E501
+        cor_list: list = data['total_routes']
+        route_val: list = page.get_total_routes()
+        assert route_val == cor_list, f'Incorrect Total routes: {data['name']}'
 
     def test_removal_strat(self, page: ProductCategory, data: dict):
-        correct_val: str = data['removal_strategy']
-        val: str = page.get_removal_strategy()
-        assert val == correct_val, f'Removal strategy is not configured correctly:{data['name']}'  # noqa: E501
+        cor_val: str = data['removal_strategy']
+        val: str | None = page.get_removal_strategy()
+        assert val == cor_val, f'Incorrect Removal strategy: {data['name']}'
 
     def test_cost_method(self, page: ProductCategory, data: dict):
         correct_val: str = data['costing_method']
         val: str = page.get_costing_methods()
-        assert val == correct_val, f'Costing method is not configured correctly:{data['name']}'  # noqa: E501
+        assert val == correct_val, f'Incorrect Costing method: {data['name']}'
 
     def test_inventory_valuation(self, page: ProductCategory, data: dict):
-        correct_val: str = data['inventory_valuation']
+        cor_val: str = data['inventory_valuation']
         val: str = page.get_valuation()
-        assert val == correct_val, f'Inventory valuation is not configured correctly:{data['name']}'  # noqa: E501
+        assert val == cor_val, f'Incorrect Inventory valuation: {data['name']}'
 
     def test_print_profile(self, page: ProductCategory, data: dict):
         correct_val: str = data['print_profile']
-        val: str = page.get_print_profile()
-        assert val == correct_val, f'Print profile is not configured correctly:{data['name']}'  # noqa: E501
+        val: str | None = page.get_print_profile()
+        assert val == correct_val, f'Incorrect Print profile: {data['name']}'
 
     def test_pallet_type(self, page: ProductCategory, data: dict):
         correct_val: str = data['pallet_type']
-        val: str = page.get_pallet_type()
-        assert val == correct_val, f'Pallet type is not configured correctly:{data['name']}'  # noqa: E501
+        val: str | None = page.get_pallet_type()
+        assert val == correct_val, f'Incorrect Pallet type: {data['name']}'
