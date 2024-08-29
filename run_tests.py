6257d80455daaab85baf3306f0f549adadbbdfce
Daveanand Mannie <daveanandmannie@gmail.com>
2024-08-28 21:40:20 -0400

    added verbosity to the runner

--- run_tests.py
+++ run_tests.py
@@ -11,33 +11,44 @@ def main():
         '-c', '--count',
         type=int,
         default=1,
-        help='Number of times to repeat the test(default = 1)'
+        help=' Number of times to repeat the test(default = 1)'
     )
 
     _ = parser.add_argument(
         '-e', '--environment',
         choices=['staging', 'production', 'uat'],
         default='staging',
-        help='Choose environemnt (default = staging)'
+        help=' Choose environemnt (default = staging)'
     )
 
     _ = parser.add_argument(
         '--tb',
         choices=['auto', 'long', 'short', 'line', 'native', 'no'],
         default='short',
-        help='Choose traceback length (default = short)'
+        help=' Choose traceback length (default = short)'
     )
 
     _ = parser.add_argument(
         '-f', '--file',
-        help='Specific test to run (default = None)'
+        help=' Specific test to run (default = None)'
     )
 
     _ = parser.add_argument(
         '--window',
         nargs='?',
         const='',
-        help='Choose to show driver opts (default = headless)'
+        help=' Choose to show driver opts (default = headless)'
+    )
+
+    _ = parser.add_argument(
+        '-v',
+        action='store_true',
+        help=' Verbosity level 1 (default = none)',
+    )
+    _ = parser.add_argument(
+        '-vv',
+        help=' Verbosity level 2 (default = none)',
+        action='store_true',
     )
 
     args = parser.parse_args()
@@ -54,6 +65,11 @@ def main():
 
     if args.window is not None:
         pytest_args.append(f'--window={args.window}')
+
+    if args.v:
+        pytest_args.append('-v')
+    elif args.vv:
+        pytest_args.append('-vv')
 
     # this is not best prac
     for _ in range(args.count):
